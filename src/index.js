27f65696c87efd765401bb3ecbbf3bb0464a2dac
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-04-29 20:32:59 +0530

    added favivon and service workers for PWA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Context from './Context/Context';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,4 +15,5 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
 
