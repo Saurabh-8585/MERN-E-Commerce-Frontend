0e46396204097ff8d998a31299af6504e8fdc569
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-05-08 13:20:33 +0530

    added responsiveness to widget

--- src/Admin/Components/AdminTabs.jsx
+++ src/Admin/Components/AdminTabs.jsx
@@ -81,29 +81,29 @@ export default function BasicTabs({ user, setUser, getUser }) {
 
     const totalRevenue = paymentData.reduce((acc, curr) => (acc + curr.totalAmount), 0);
     const isSmallScreen = useMediaQuery('(max-width:600px)');
-    
+
     return (
         <Box sx={{ width: '100%' }}>
             <Grid container spacing={2} direction={isSmallScreen ? 'column' : 'row'} padding={1}>
-                <Grid item xs={12} sm={6} md={6}>
+                <Grid item xs={12} sm={6} md={6} lg={3}>
                     <Widget numbers={totalRevenue} heading='Revenue' color='#9932CC' icon={<TbReportMoney />} />
                 </Grid>
-                <Grid item xs={12} sm={6} md={6}>
+                <Grid item xs={12} sm={6} md={6} lg={3}>
                     <Widget numbers={products.length} heading='Products' color='#FFC300' icon={<AiOutlineShoppingCart />} />
                 </Grid>
-                <Grid item xs={12} sm={6} md={6}>
+                <Grid item xs={12} sm={6} md={6} lg={3}>
                     <Widget numbers={user.length} heading='Users' color='#FF69B4' icon={<CgProfile />} />
                 </Grid>
-                <Grid item xs={12} sm={6} md={6}>
+                <Grid item xs={12} sm={6} md={6} lg={3}>
                     <Widget numbers={paymentData.length} heading='Orders' color='#1f77b4  ' icon={<FaShippingFast />} />
                 </Grid>
             </Grid>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'center', alignItems: 'center',marginTop:5 }}>
-                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" style={{overflowX:"a"}} >
-                    <Tab label={!isSmallScreen &&'Statistics'}  {...a11yProps(0)} iconPosition='start' icon={<VscGraph fontSize={20} />} />
-                    <Tab label={!isSmallScreen &&"Users"} {...a11yProps(1)} iconPosition='start' icon={<CgProfile fontSize={20} />} />
-                    <Tab label={!isSmallScreen &&"Products"} {...a11yProps(2)} iconPosition='start' icon={<AiOutlineShoppingCart fontSize={20} />} />
-                    <Tab label={!isSmallScreen &&"Orders"} {...a11yProps(3)} iconPosition='start' icon={<FaShippingFast fontSize={20} />} />
+            <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 5 }}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" style={{ overflowX: "a" }} >
+                    <Tab label={!isSmallScreen && 'Statistics'}  {...a11yProps(0)} iconPosition='start' icon={<VscGraph fontSize={20} />} />
+                    <Tab label={!isSmallScreen && "Users"} {...a11yProps(1)} iconPosition='start' icon={<CgProfile fontSize={20} />} />
+                    <Tab label={!isSmallScreen && "Products"} {...a11yProps(2)} iconPosition='start' icon={<AiOutlineShoppingCart fontSize={20} />} />
+                    <Tab label={!isSmallScreen && "Orders"} {...a11yProps(3)} iconPosition='start' icon={<FaShippingFast fontSize={20} />} />
                 </Tabs>
             </Box>
             <TabPanel value={value} index={0} >
